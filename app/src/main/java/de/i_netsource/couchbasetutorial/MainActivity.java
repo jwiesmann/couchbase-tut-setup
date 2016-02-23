72ff4f4ff73ba466c04af311d89eb52bd97b8286
jwiesmann <joerg.wiesmann@gmail.com>
2016-02-23 13:41:48 +0100

    couchbase tutorial setup

--- app/src/main/java/de/i_netsource/couchbasetutorial/MainActivity.java
+++ app/src/main/java/de/i_netsource/couchbasetutorial/MainActivity.java
@@ -3,13 +3,11 @@ package de.i_netsource.couchbasetutorial;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import de.i_netsource.couchbasetutorial.activity.MasterActivity;
 import de.i_netsource.couchbasetutorial.db.DBOperations;
 
 public class MainActivity extends DBOperations {
